8d7a876cf6a25c3f754b267c72adebf8fdf59062
Shefket Bylykbashi <94128957+shefketbylykbashi@users.noreply.github.com>
2025-11-27 08:13:53 +0100

    Add files via upload

---
+++ ProgramItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouTubeInstanceGenerator.Models
+{
+    public class ProgramItem
+    {
+        public string program_id { get; set; }
+        public int start { get; set; }
+        public int end { get; set; }
+        public string genre { get; set; }
+        public int score { get; set; }
+        public string? link { get; set; }
+    }
+}
